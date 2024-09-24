37c7648da79f0a8699b65adc75091cc790a4bcf2
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2024-09-24 20:55:19 +0100

    add toasts if there is an error doing auth stuff

--- frontend/src/app/(auth)/components/AuthFormButton.tsx
+++ frontend/src/app/(auth)/components/AuthFormButton.tsx
@@ -3,6 +3,9 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useFormStatus } from "react-dom";
 import { Spinner } from "./Spinner";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 const buttonCopy = {
 	idle: "Send me a login link",
 	pending: <Spinner size={16} color="rgba(255, 255, 255, 0.65)" />,
@@ -10,14 +13,26 @@ const buttonCopy = {
 };
 export function SubmitButton({ state }: { state: { status: string } }) {
 	const { pending } = useFormStatus();
+	const router = useRouter()
 	const buttonState = state.status === "idle" ? state.status : pending ? "pending" : "success";
+	if (buttonState === "success") {
+		setTimeout(() => {
+			router.push("/")
+		}, 1000);
+	}
+
+	useEffect(() => {
+		if (state.status === "error") {
+		toast.error("An error occurred")
+	}
+	},[state])
 	return (
 		<motion.button
 			type="submit"
 			className="w-full rounded-md border-4 border-stone-800 bg-stone-200 px-4 py-2 font-bold text-lg text-stone-800 shadow-[4px_4px_0px_0px_rgba(28,25,23,1)] hover:bg-stone-300 focus:outline-none focus:ring-2 focus:ring-stone-500 focus:ring-offset-2"
 			whileHover={{ scale: 1.02 }}
 			whileTap={{ scale: 0.98 }}
-			disabled={buttonState !== "idle"}
+			disabled={state.status === "error" || buttonState !== "idle"}
 		>
 			<AnimatePresence mode="popLayout" initial={false}>
 				<motion.span
