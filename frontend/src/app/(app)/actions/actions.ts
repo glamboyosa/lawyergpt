fa53f7b0e89786d0e5caa2a70790e7561cffcdec
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2024-09-26 13:09:23 +0100

    move server only to the top

--- frontend/src/app/(app)/actions/actions.ts
+++ frontend/src/app/(app)/actions/actions.ts
@@ -1,4 +1,5 @@
 "use server";
+import "server-only";
 import { hasAuthCookie } from "@/lib/auth";
 import { db } from "@/lib/db";
 import {
@@ -6,7 +7,6 @@ import {
   insertConvoSchema,
 } from "@/lib/db/schema/conversations";
 import { redirect } from "next/navigation";
-import "server-only";
 
 export async function createNewConversation(userId: string) {
   const isAllowed = hasAuthCookie();
